afff603fb49df3b5d195d46c156e5df7f1e5c741
Elvin <109756203+ElvinRustamov@users.noreply.github.com>
2024-03-07 22:41:30 +0400

    Add files via upload

---
+++ C++ (Learning)/PrimitiveDataTypes.cpp
@@ -0,0 +1,30 @@
+/*
+Primitive Data Types:
+- Character types
+- Integer types (signed -> range negative to positive and unsigned -> from 0 to positive)
+- Floating-point types
+- Boolean type
+*/
+
+#include <iostream>
+
+int main () {
+//    char middle_initial = 'J'; //notice the single quotes around characters
+//    std::cout << middle_initial;
+
+//    unsigned short int exam_score = 55; // same as unsigned short exam_score = 55;
+//    std::cout << "Exam score was " << exam_score;
+
+//    long long people_on_earth = 7'600'000'000;
+//    std::cout << "There are about " << people_on_earth << " people on earth" << std::endl;
+
+//    float car_payment = 401.23;
+//    std::cout << "My car payment is " << car_payment;
+
+//    long double large_amount = 2.7e10;
+//    std::cout << large_amount << " is very big number";
+
+    bool game_over {false};
+    std::cout << "The value of game_over is " << game_over;
+    return 0;
+}
