80ddb52a987eab5077eeeec83dbd4b1254a1ebb2
Elvin <109756203+ElvinRustamov@users.noreply.github.com>
2024-06-28 00:36:41 +0400

    Add files via upload

---
+++ Data Structures and Algorithms/Grokking Algorithms (Book by Aditya Y.Bhargava)/Python V./Breadth-first.py
@@ -0,0 +1,37 @@
+# Breadth-first search algorithm
+
+# The algorithm to solve a shortest-path problem is called breadth-first search.
+
+from collections import deque
+
+graph = {}
+graph['you'] = ['alice', 'bob', 'claire']
+graph['bob'] = ['anuj', 'peggy']
+graph['alice'] = ['peggy']
+graph['claire'] = ['thom', 'jonny']
+graph['anuj'] = []
+graph['peggy'] = []
+graph['thom'] = []
+graph['jonny'] = []
+
+
+
+def person_is_seller(person):
+    return (person[-1] == 'm')
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(f"{person} is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+search('you')
